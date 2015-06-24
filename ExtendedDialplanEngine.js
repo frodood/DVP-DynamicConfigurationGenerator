1f007fbea0554bf621eaf1f7b0c0f3303a8667cb
dinusha_dck <dinusha@duosoftware.com>
2015-06-24 15:39:58 +0530

    add package.json

--- ExtendedDialplanEngine.js
+++ ExtendedDialplanEngine.js
@@ -575,7 +575,7 @@ var ProcessExtendedDialplan = function(reqId, ani, dnis, context, direction, ext
                                                 }
                                                 else if(pbxObj.OperationType === 'CALL_DIVERT')
                                                 {
-                                                    //Check if divert number
+                                                    //Check if divert number is an extension or not
                                                     callback(err, xmlBuilder.createNotFoundResponse());
                                                 }
                                                 else

61cdbbe83f3f2fcec11fcb17bb8933b21d0ac330
dinusha_dck <dinusha@duosoftware.com>
2015-07-23 09:52:08 +0530

    correct conference picking query

--- SipExtBackendOperations.js
+++ SipExtBackendOperations.js
@@ -160,7 +160,7 @@ var GetAllDataForExt = function(reqId, extension, tenantId, extType, callback)
         }
         else if(extType === 'CONFERENCE')
         {
-            dbModel.Extension.find({where: [{Extension: extension},{TenantId: tenantId},{ObjCategory: extType}], include: [{model: dbModel.Conference, as:'Conference', include : [{model: dbModel.ConferenceUser, as : 'ConferenceUser', include:[{model: dbModel.SipUACEndpoint, as: 'SipUACEndpoint'}]}]},{model: dbModel.CloudEndUser, as: 'CloudEndUser'}]})
+            dbModel.Extension.find({where: [{Extension: extension},{TenantId: tenantId},{ObjCategory: extType}], include: [{model: dbModel.Conference, as:'Conference', include : [{model: dbModel.ConferenceUser, as : 'ConferenceUser', include:[{model: dbModel.SipUACEndpoint, as: 'SipUACEndpoint'}]},{model: dbModel.CloudEndUser, as: 'CloudEndUser'}]}]})
                 .complete(function (err, extData)
                 {
                     callback(err, extData);

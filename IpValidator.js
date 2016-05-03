0c3f626c57e2c9f712ec7cdfe974032c9eaf75e8
dinusha_dck <dinusha@duosoftware.com>
2016-05-03 10:38:36 +0530

    add ip validation

--- IpValidator.js
+++ IpValidator.js
@@ -3,20 +3,32 @@
  */
 
 var ip = require('ip');
+var validator = require('validator');
 
 var ValidateRange = function(ipToValidate, ipRangeListWithSubnet)
 {
     for (i = 0; i < ipRangeListWithSubnet.length; i++)
     {
-        if(ipRangeListWithSubnet[i].IpAddress && ipRangeListWithSubnet[i].Mask)
+        if(validator.isIP(ipToValidate))
         {
-            var ipRange = ipRangeListWithSubnet[i].IpAddress + '/' + ipRangeListWithSubnet[i].Mask;
+            if(ipRangeListWithSubnet[i].IpAddress && ipRangeListWithSubnet[i].Mask)
+            {
+                var ipRange = ipRangeListWithSubnet[i].IpAddress + '/' + ipRangeListWithSubnet[i].Mask;
 
-            if(ip.cidrSubnet(ipRange).contains(ipToValidate))
+                if(ip.cidrSubnet(ipRange).contains(ipToValidate))
+                {
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            if(ipToValidate === ipRangeListWithSubnet[i].IpAddress)
             {
                 return true;
             }
         }
+
 
     }
     return false;

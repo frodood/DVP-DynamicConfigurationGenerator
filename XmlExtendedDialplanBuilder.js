bdf52d75670b4b4ef9d3428b9a40bca990f4f326
dinusha_dck <dinusha@duosoftware.com>
2017-02-14 14:21:01 +0530

    revert change

--- XmlExtendedDialplanBuilder.js
+++ XmlExtendedDialplanBuilder.js
@@ -101,7 +101,7 @@ var CreatePbxFeatures = function(reqId, destNum, pbxType, domain, trunkNumber, t
 
         var cond = doc.att('type', 'freeswitch/xml')
             .ele('section').att('name', 'dialplan').att('description', 'RE Dial Plan For FreeSwitch')
-            .ele('context').att('name', 'PBXFeatures')
+            .ele('context').att('name', context)
             .ele('extension').att('name', destNum)
             .ele('condition').att('field', 'destination_number').att('expression', '^' + destNum + '$')
 
@@ -137,7 +137,6 @@ var CreatePbxFeatures = function(reqId, destNum, pbxType, domain, trunkNumber, t
         }
         else
         {
-            context = 'PBXFeatures';
             cond.ele('action').att('application', 'read').att('data', "3 6 'tone_stream://%(10000,0,350,440)' digits 30000 #")
                 .up()
                 .ele('action').att('application', 'set').att('data', 'origination_cancel_key=#')
